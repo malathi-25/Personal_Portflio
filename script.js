766ccf0ceeda12499850cbf9d948112aabd06878
malathi-25 <firefacterfly@gmail.com>
2026-01-10 18:25:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function copyText(text) {
+    navigator.clipboard.writeText(text);
+    alert("Copied: " + text);
+}
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+    reveals.forEach(el => {
+        const top = el.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+        if (top < windowHeight - 100) {
+            el.classList.add("active");
+        }
+    });
+});
 
